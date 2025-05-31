7c1b59ea7f2875a0487debfb04b768abc5c84091
Moyses-Zerbieti <zerbietimoyses@gmail.com>
2025-05-31 19:53:58 -0300

    feat: mapear entidade FuncionarioEntity conforme estrutura da tabela no PostgreSQL -Entidade JPA com campos anotados para refletir a tabela 'funcionarios'. Inclui enum SetorTrabalho e UUID para matrícula.

--- sistemQrCode/src/main/java/com/codigoQR/sistemQrCode/model/FuncionarioEntity.java
+++ sistemQrCode/src/main/java/com/codigoQR/sistemQrCode/model/FuncionarioEntity.java
@@ -1,65 +1,42 @@
 package com.codigoQR.sistemQrCode.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
 
 @Entity
-@Table (name = "funcionarios")
+@Table (name = "funcionarios", schema = "public")
+@Getter
+@Setter
 public class FuncionarioEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column
+    @Column(name = "id")
     private Integer id;
 
-    @Column
-    private  String nome;
+    @Column(name = "nome_completo", length = 100, nullable = false)
+    private  String nomeCompleto;
 
-    @Column
+    @Column(name = "cpf", length = 14, nullable = false, unique = true)
     private  String cpf;
 
-    @Column
-    private String email;
+    @Column(name = "data_nascimento", nullable = false)
+    private LocalDate dataNascimento;
 
-    @Column
+    @Column(name = "matricula", nullable = false, unique = true)
+    private UUID matricula;
+
+    @Column(name = "email_corporativo",length = 100,nullable = false,unique = true)
+    private String emailCorporativo;
+
+    @Column(name = "cargo", length = 20, nullable = false)
     private String cargo;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getCargo() {
-        return cargo;
-    }
-
-    public void setCargo(String cargo) {
-        this.cargo = cargo;
-    }
+    @Enumerated(EnumType.STRING)
+    @Column(name = "setor", length = 30,nullable = false)
+    private SetorTrabalho setor;
 }
