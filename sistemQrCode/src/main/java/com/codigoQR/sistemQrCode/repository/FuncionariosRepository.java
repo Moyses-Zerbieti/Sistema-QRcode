45a447e04408a42e01cdee68090cdbc6e75ff115
Moyses-Zerbieti <zerbietimoyses@gmail.com>
2025-06-04 20:00:57 -0300

    feat(repository): adicionar métodos personalizados em FuncionariosRepository
    - Adicionados métodos para verificar existência de CPF e email corporativo.
    - Incluídos métodos para buscar FuncionarioEntity por CPF e email corporativo.
    - Facilita validação e consultas específicas no banco de dados.

--- sistemQrCode/src/main/java/com/codigoQR/sistemQrCode/repository/FuncionariosRepository.java
+++ sistemQrCode/src/main/java/com/codigoQR/sistemQrCode/repository/FuncionariosRepository.java
@@ -2,6 +2,11 @@ package com.codigoQR.sistemQrCode.repository;
 import com.codigoQR.sistemQrCode.model.FuncionarioEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FuncionariosRepository extends JpaRepository<FuncionarioEntity, Integer> {
 
+public interface FuncionariosRepository extends JpaRepository<FuncionarioEntity, Integer> {
+    boolean existsByCpf(String cpf);
+    boolean existsByEmailCorporativo(String emailCorporativo);
+
+    FuncionarioEntity findByCpf(String cpf);
+    FuncionarioEntity findByEmailCorporativo(String emailCorporativo);
 }
