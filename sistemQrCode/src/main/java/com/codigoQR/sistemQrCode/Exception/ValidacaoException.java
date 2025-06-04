966e5c772515c52c535a3dbb3b9bf7bd73ed59ed
Moyses-Zerbieti <zerbietimoyses@gmail.com>
2025-06-04 20:10:24 -0300

    feat(exception): adicionar exceção personalizada ValidacaoException
    - Cria classe `ValidacaoException` que estende `RuntimeException`.
    - Utilizada para representar erros de validação de regras de negócio, como tentativas de alteração de CPF ou e-mail.

--- sistemQrCode/src/main/java/com/codigoQR/sistemQrCode/Exception/ValidacaoException.java
+++ sistemQrCode/src/main/java/com/codigoQR/sistemQrCode/Exception/ValidacaoException.java
@@ -3,5 +3,6 @@ package com.codigoQR.sistemQrCode.Exception;
 public class ValidacaoException extends RuntimeException{
     public ValidacaoException (String mensagem){
         super(mensagem);
+
     }
 }
