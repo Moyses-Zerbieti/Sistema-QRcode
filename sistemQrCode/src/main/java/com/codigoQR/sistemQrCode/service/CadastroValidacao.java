b8cd6d58c9b5270b8d62c520614201d385107669
Moyses-Zerbieti <zerbietimoyses@gmail.com>
2025-06-04 19:54:15 -0300

    feat(service): adicionar validação para cadastro de funcionário em CadastroValidacao
    Implementa verificação para impedir cadastro de funcionários com CPF ou e-mail corporativo já existentes, lançando exceção personalizada em caso de duplicidade.

--- sistemQrCode/src/main/java/com/codigoQR/sistemQrCode/service/CadastroValidacao.java
+++ sistemQrCode/src/main/java/com/codigoQR/sistemQrCode/service/CadastroValidacao.java
@@ -1,5 +1,7 @@
 package com.codigoQR.sistemQrCode.service;
 
+import com.codigoQR.sistemQrCode.Exception.ValidacaoException;
+import com.codigoQR.sistemQrCode.model.FuncionarioEntity;
 import com.codigoQR.sistemQrCode.repository.FuncionariosRepository;
 import org.springframework.stereotype.Component;
 
@@ -10,5 +12,13 @@ public class CadastroValidacao {
 
     public CadastroValidacao(FuncionariosRepository repository){
         this.repository = repository;
+    }
+    public void validarFuncionario(FuncionarioEntity funcionario){
+        if (repository.existsByCpf(funcionario.getCpf())) {
+            throw new ValidacaoException("CPF já cadastrado.");
+        }
+        if (repository.existsByEmailCorporativo(funcionario.getEmailCorporativo())){
+            throw new ValidacaoException("Email corporativo já cadastrado.");
+        }
     }
 }
