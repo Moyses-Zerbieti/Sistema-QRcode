26984d5b384a57e07e107e4e0428b1f3a6ca889a
Moyses-Zerbieti <zerbietimoyses@gmail.com>
2025-07-01 21:40:59 -0300

    feat: adicionar ServiceAcesso para processar escaneamento de QR Code com controle de acesso baseado em tempo

---
+++ sistemQrCode/src/main/java/com/codigoQR/sistemQrCode/service/ServiceAcesso.java
@@ -0,0 +1,52 @@
+package com.codigoQR.sistemQrCode.service;
+
+import com.codigoQR.sistemQrCode.model.FuncionarioEntity;
+import com.codigoQR.sistemQrCode.model.RegistroAcessoEntity;
+import com.codigoQR.sistemQrCode.repository.FuncionariosRepository;
+import com.codigoQR.sistemQrCode.repository.RegistroAcessoRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class ServiceAcesso {
+    private final FuncionariosRepository funcionariosRepository;
+    private final RegistroAcessoRepository registroAcessoRepository;
+
+    public ServiceAcesso(
+            FuncionariosRepository funcionariosRepository,RegistroAcessoRepository registroAcessoRepository){
+
+        this.funcionariosRepository = funcionariosRepository;
+        this.registroAcessoRepository = registroAcessoRepository;
+    }
+
+    public Map<String, Object> processarEscaneamento(Integer idFuncionario){
+        FuncionarioEntity funcionario = funcionariosRepository.findById(idFuncionario)
+                .orElseThrow(() -> new RuntimeException("Funcionário não encontrado."));
+
+        boolean acessoNegado = registroAcessoRepository.findUltimoAcesso(funcionario.getCpf())
+                .filter(reg -> Duration.between(reg.getDataHoraAcesso(), LocalDateTime.now())
+                .toMinutes() < 120)
+                .isPresent();
+
+        RegistroAcessoEntity registroAcesso = new RegistroAcessoEntity();
+        registroAcesso.setDataHoraAcesso(LocalDateTime.now());
+        registroAcesso.setStatus(acessoNegado ? "NEGADO" : "AUTORIZADO");
+        registroAcesso.setSetor(funcionario.getSetor());
+        registroAcesso.setFuncionario(funcionario);
+        registroAcessoRepository.save(registroAcesso);
+
+        Map<String, Object> resposta = new HashMap<>();
+        resposta.put("nome", funcionario.getNomeCompleto());
+        resposta.put("cargo", funcionario.getCargo());
+        resposta.put("setor", funcionario.getSetor());
+        resposta.put("status", registroAcesso.getStatus());
+        resposta.put("mensagem", acessoNegado ?
+                "Acesso negado! Aguarde pelo menos 1 hora." :
+                "Acesso autorizado para " + funcionario.getNomeCompleto());
+        return resposta;
+    }
+}
