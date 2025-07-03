b4fdfcf58fad8000d0d3109c6f8a581067167ad7
Moyses-Zerbieti <zerbietimoyses@gmail.com>
2025-07-03 01:14:09 -0300

    chore: Removida a restrição CHECK no campo setor da tabela funcionarios para permitir maior flexibilidade nos valores armazenados.

--- sistemQrCode/funcionarios.sql
+++ sistemQrCode/funcionarios.sql
@@ -6,6 +6,5 @@ data_nascimento date not null,
 matricula uuid not null unique,
 email_corporativo varchar(100) not null unique,
 cargo varchar(20) not null,
-setor varchar(30) not null,
-constraint chk_setor check(setor in ('PRODUCAO', 'SUPERVISAO', 'GERENCIA', 'DIRETORIA', 'RECURSOS_HUMANOS'))
+setor varchar(30) not null
 );
