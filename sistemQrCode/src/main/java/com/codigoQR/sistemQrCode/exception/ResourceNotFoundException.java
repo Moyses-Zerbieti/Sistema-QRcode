ff43d4a71b9921e35468c9560a0c9320640b2470
Moyses-Zerbieti <zerbietimoyses@gmail.com>
2025-07-03 01:00:47 -0300

    refactor: alterando modo de escrita do pacote exceptionw

--- sistemQrCode/src/main/java/com/codigoQR/sistemQrCode/exception/ResourceNotFoundException.java
+++ sistemQrCode/src/main/java/com/codigoQR/sistemQrCode/exception/ResourceNotFoundException.java
@@ -1,4 +1,4 @@
-package com.codigoQR.sistemQrCode.Exception;
+package com.codigoQR.sistemQrCode.exception;
 
 public class ResourceNotFoundException extends RuntimeException{
     public ResourceNotFoundException(String message){
