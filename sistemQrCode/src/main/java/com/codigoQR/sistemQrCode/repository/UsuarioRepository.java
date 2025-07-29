e25eeee612582684b84670ec297d00479a3d0239
Moyses-Zerbieti <zerbietimoyses@gmail.com>
2025-07-29 19:45:41 -0300

    feat(usuario): cria repositório UsuarioRepository com busca por login
    - Implementa interface JpaRepository para entidade Usuario
    - Adiciona método findByLogin(String login) para facilitar autenticação

---
+++ sistemQrCode/src/main/java/com/codigoQR/sistemQrCode/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package com.codigoQR.sistemQrCode.repository;
+
+import com.codigoQR.sistemQrCode.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UsuarioRepository extends JpaRepository <Usuario,UUID>{
+    Usuario findByLogin(String login);
+}
