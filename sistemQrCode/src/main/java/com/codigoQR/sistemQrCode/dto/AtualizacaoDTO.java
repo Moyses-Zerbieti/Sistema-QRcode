060b216a60fefcd3b6d544eb499dfa8cf18f080b
Moyses-Zerbieti <zerbietimoyses@gmail.com>
2025-07-30 15:08:50 -0300

    feat: cria AtualizacaoDTO para controle seguro de dados atualizáveis do funcionário
    - Define estrutura específica para atualização de funcionário
    - Restringe mudanças aos campos nomeCompleto, cargo e setor
    - Evita alterações críticas como CPF, e-mail ou matrícula

---
+++ sistemQrCode/src/main/java/com/codigoQR/sistemQrCode/dto/AtualizacaoDTO.java
@@ -0,0 +1,35 @@
+package com.codigoQR.sistemQrCode.dto;
+
+
+public class AtualizacaoDTO {
+
+    private String nomeCompleto;
+
+    private String cargo;
+
+    private String setor;
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getSetor() {
+        return setor;
+    }
+
+    public void setSetor(String setor) {
+        this.setor = setor;
+    }
+}
