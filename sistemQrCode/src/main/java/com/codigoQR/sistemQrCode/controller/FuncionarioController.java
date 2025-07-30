d81c53ad7d2e2b046df3eac134cddcf4c772eabd
Moyses-Zerbieti <zerbietimoyses@gmail.com>
2025-07-30 15:03:54 -0300

    refactor: adiciona segurança e padroniza DTOs no FuncionarioController
    - Substituído ServiceCadastro por ServiceFuncionarios
    - Adicionados filtros de segurança com @PreAuthorize nos endpoints
    - Implementado uso de AtualizacaoDTO para atualização de funcionários
    - Adicionado retorno com FuncionarioResponseDTO para padronizar respostas
    - Melhoria na manutenção e segurança do sistema de cadastro

--- sistemQrCode/src/main/java/com/codigoQR/sistemQrCode/controller/FuncionarioController.java
+++ sistemQrCode/src/main/java/com/codigoQR/sistemQrCode/controller/FuncionarioController.java
@@ -1,24 +1,27 @@
 package com.codigoQR.sistemQrCode.controller;
 
+import com.codigoQR.sistemQrCode.dto.AtualizacaoDTO;
 import com.codigoQR.sistemQrCode.dto.FuncionarioRequest;
+import com.codigoQR.sistemQrCode.dto.FuncionarioResponseDTO;
 import com.codigoQR.sistemQrCode.model.FuncionarioEntity;
-import com.codigoQR.sistemQrCode.service.ServiceCadastro;
+import com.codigoQR.sistemQrCode.service.ServiceFuncionarios;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 
 @RestController
 @RequestMapping("funcionario")
 public class FuncionarioController implements GenericController{
-    private ServiceCadastro service;
-    public FuncionarioController(ServiceCadastro service) {
+    private ServiceFuncionarios service;
+    public FuncionarioController(ServiceFuncionarios service) {
         this.service = service;
     }
 
     @PostMapping
+    @PreAuthorize("hasAnyRole('ADMIN', 'STAFF')")
     public ResponseEntity<?> salvar(@Valid @RequestBody FuncionarioRequest funcionarioRequest) {
         FuncionarioEntity salvo = service.salvar(funcionarioRequest);
         URI location = gerarHeaderLocation(salvo.getId());
@@ -28,17 +31,36 @@ public class FuncionarioController implements GenericController{
     }
 
     @GetMapping("{id}")
-    public FuncionarioEntity consultaId(@PathVariable Integer id){
+    @PreAuthorize("hasAnyRole('ADMIN','STAFF','USER')")
+    public FuncionarioResponseDTO consultaId(@PathVariable Integer id){
         return service.consultaId(id);
     }
 
-    @PutMapping
-    public ResponseEntity<FuncionarioEntity> atualizar(@Valid @RequestBody FuncionarioEntity funcionario){
-        FuncionarioEntity atualizado = service.atualizarInformacoes(funcionario);
-        return ResponseEntity.ok(atualizado);
+    @PutMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN', 'STAFF')")
+    public ResponseEntity<FuncionarioResponseDTO> atualizar(
+            @PathVariable Integer id,
+            @Valid @RequestBody AtualizacaoDTO dto){
+
+        FuncionarioEntity atualizado = service.atualizarInformacoes(id,dto);
+
+        FuncionarioResponseDTO response = new FuncionarioResponseDTO(
+                atualizado.getId(),
+                atualizado.getNomeCompleto(),
+                atualizado.getCpf(),
+                atualizado.getDataNascimento(),
+                atualizado.getMatricula(),
+                atualizado.getEmailCorporativo(),
+                atualizado.getCargo(),
+                atualizado.getSetor(),
+                atualizado.getDataCadastro(),
+                atualizado.getDataAtualizacao()
+        );
+        return ResponseEntity.ok(response);
     }
 
     @DeleteMapping("{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<?> deletarFuncionario(@PathVariable("id") Integer id){
             service.deletarFuncionario(id);
             return ResponseEntity.ok("Funcionário com id " + id + " foi deletado com sucesso.");
