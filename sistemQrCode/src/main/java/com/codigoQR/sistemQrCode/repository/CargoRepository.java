1ade302356da707f10d3c18b08f5dc68d3117473
Moyses-Zerbieti <zerbietimoyses@gmail.com>
2025-08-10 20:39:38 -0300

    feat: criar repositório para entidade Cargo
    Adicionado o `CargoRepository` estendendo `JpaRepository` para permitir operações CRUD e consultas personalizadas sobre a entidade `Cargo`.
    
    - Interface criada com tipagem `Cargo` e chave primária `Integer`.
    - Permite integração direta com o banco de dados utilizando Spring Data JPA.

---
+++ sistemQrCode/src/main/java/com/codigoQR/sistemQrCode/repository/CargoRepository.java
@@ -0,0 +1,8 @@
+package com.codigoQR.sistemQrCode.repository;
+
+import com.codigoQR.sistemQrCode.model.Cargo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CargoRepository extends JpaRepository<Cargo,Integer> {
+
+}
