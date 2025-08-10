7c2abe099edfda73a66bfd79a3714bdf43cdc9f1
Moyses-Zerbieti <zerbietimoyses@gmail.com>
2025-08-10 20:42:52 -0300

    feat: criar repositório JPA para entidade Setor
    Adicionado `SetorRepository` estendendo `JpaRepository` para fornecer operações CRUD e consultas para a entidade `Setor`.
    
    - Implementação baseada em Spring Data JPA.
    - Utiliza `Integer` como tipo do identificador.
    - Permite futura criação de consultas personalizadas, mantendo as operações básicas já disponíveis.

---
+++ sistemQrCode/src/main/java/com/codigoQR/sistemQrCode/repository/SetorRepository.java
@@ -0,0 +1,8 @@
+package com.codigoQR.sistemQrCode.repository;
+
+import com.codigoQR.sistemQrCode.model.Setor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SetorRepository extends JpaRepository <Setor,Integer>{
+
+}
