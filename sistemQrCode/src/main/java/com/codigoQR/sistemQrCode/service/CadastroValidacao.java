1462f356fdbc41aa40225856a0e825c13af69776
Moyses-Zerbieti <zerbietimoyses@gmail.com>
2025-09-23 20:20:25 -0300

    feat: adicionar validação de veículos em CadastroValidacao

--- sistemQrCode/src/main/java/com/codigoQR/sistemQrCode/service/CadastroValidacao.java
+++ sistemQrCode/src/main/java/com/codigoQR/sistemQrCode/service/CadastroValidacao.java
@@ -2,23 +2,34 @@ package com.codigoQR.sistemQrCode.service;
 
 import com.codigoQR.sistemQrCode.exception.ValidacaoException;
 import com.codigoQR.sistemQrCode.model.Funcionario;
+import com.codigoQR.sistemQrCode.model.Veiculo;
 import com.codigoQR.sistemQrCode.repository.FuncionarioRepository;
+import com.codigoQR.sistemQrCode.repository.VeiculoRepository;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CadastroValidacao {
 
     private FuncionarioRepository repository;
+    private VeiculoRepository veiculoRepository;
 
-    public CadastroValidacao(FuncionarioRepository repository){
+    public CadastroValidacao(FuncionarioRepository repository, VeiculoRepository veiculoRepository){
         this.repository = repository;
+        this.veiculoRepository = veiculoRepository;
     }
+
     public void validarFuncionario(Funcionario funcionario){
         if (repository.existsByCpf(funcionario.getCpf())) {
             throw new ValidacaoException("CPF já cadastrado.");
         }
         if (repository.existsByEmailCorporativo(funcionario.getEmailCorporativo())){
             throw new ValidacaoException("Email corporativo já cadastrado.");
+        }
+    }
+
+    public void validarCadastroVeiculo(Veiculo veiculo){
+        if (veiculoRepository.existsByPlaca(veiculo.getPlaca())){
+            throw new ValidacaoException("Veiculo com placa " + veiculo.getPlaca() + " já cadastrado");
         }
     }
 }
