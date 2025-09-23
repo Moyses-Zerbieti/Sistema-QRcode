5c40ee42fb7b8c8ef8c248d37b2f42870ff3c9a6
Moyses-Zerbieti <zerbietimoyses@gmail.com>
2025-09-23 20:19:52 -0300

    feat: adicionar validações para veículos em AtualizarDadosValidacao

--- sistemQrCode/src/main/java/com/codigoQR/sistemQrCode/service/AtualizarDadosValidacao.java
+++ sistemQrCode/src/main/java/com/codigoQR/sistemQrCode/service/AtualizarDadosValidacao.java
@@ -2,7 +2,9 @@ package com.codigoQR.sistemQrCode.service;
 
 import com.codigoQR.sistemQrCode.exception.ResourceNotFoundException;
 import com.codigoQR.sistemQrCode.model.Funcionario;
+import com.codigoQR.sistemQrCode.model.Veiculo;
 import com.codigoQR.sistemQrCode.repository.FuncionarioRepository;
+import com.codigoQR.sistemQrCode.repository.VeiculoRepository;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,13 +12,23 @@ public class AtualizarDadosValidacao {
 
     private FuncionarioRepository repository;
 
-    public AtualizarDadosValidacao(FuncionarioRepository repository) {
+    private VeiculoRepository veiculoRepository;
+
+
+    public AtualizarDadosValidacao(FuncionarioRepository repository,VeiculoRepository veiculoRepository){
         this.repository = repository;
+        this.veiculoRepository = veiculoRepository;
     }
 
     public void validarExisteFuncionario(Integer id){
         if (!repository.existsById(id)){
             throw new ResourceNotFoundException("Funcionário com ID " + id + " não encontrado.");
+        }
+    }
+
+    public void validarExisteVeiculo(Integer id){
+        if(!veiculoRepository.existsById(id)){
+            throw new ResourceNotFoundException("Veículo com ID " + id + " não encontrado");
         }
     }
 
@@ -33,9 +45,25 @@ public class AtualizarDadosValidacao {
         }
     }
 
+    public void validarPlacasDuplas(Veiculo veiculo){
+        Veiculo existePlaca = veiculoRepository.findByPlaca(veiculo.getPlaca());
+
+        boolean placaDuplicada  = existePlaca != null && !existePlaca.getId().equals(veiculo.getId());
+
+        if (placaDuplicada){
+            throw new RuntimeException("Já existe um veiculo com essa placa cadastrada");
+        }
+
+    }
+
     public Funcionario buscarOuFalhar(Integer id){
         return repository.findById(id)
-                .orElseThrow(() ->new ResourceNotFoundException("Funcionário com id "  + id + " não encontrado."));
+                .orElseThrow(() -> new ResourceNotFoundException("Funcionário com id "  + id + " não encontrado."));
 
+    }
+
+    public Veiculo buscarOuFalharVeiculo(Integer id){
+        return veiculoRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Veículo com id " + id + "não encontrado."));
     }
 }
