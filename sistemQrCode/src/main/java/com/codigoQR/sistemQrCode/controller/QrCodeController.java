df97e7fd5f21b8448760f090985df3ab23d393c6
Moyses-Zerbieti <zerbietimoyses@gmail.com>
2025-09-23 20:17:42 -0300

    refactor: padronizar QrCodeController e implementar @PreAuthorize para controle de acesso

--- sistemQrCode/src/main/java/com/codigoQR/sistemQrCode/controller/QrCodeController.java
+++ sistemQrCode/src/main/java/com/codigoQR/sistemQrCode/controller/QrCodeController.java
@@ -1,7 +1,9 @@
 package com.codigoQR.sistemQrCode.controller;
 
-import com.codigoQR.sistemQrCode.service.ServiceAcesso;
+import com.codigoQR.sistemQrCode.genericController.GenericControllers;
+import com.codigoQR.sistemQrCode.service.AcessoService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,16 +13,17 @@ import java.net.URI;
 
 @RestController
 @RequestMapping("qrcode")
-public class QrCodeController implements GenericController {
-    private ServiceAcesso serviceAcesso;
+public class QrCodeController implements GenericControllers {
+    private AcessoService acessoService;
 
-    public QrCodeController(ServiceAcesso serviceAcesso) {
-        this.serviceAcesso = serviceAcesso;
+    public QrCodeController(AcessoService acessoService) {
+        this.acessoService = acessoService;
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("escanear/{id}")
     public ResponseEntity<?> escanearQrCode(@PathVariable Integer id){
-        URI location =gerarHeaderLocation(id);
-        return ResponseEntity.ok(serviceAcesso.processarEscaneamento(id));
+        URI location = gerarHeaderLocation(id);
+        return ResponseEntity.ok(acessoService.processarEscaneamento(id));
     }
 }
