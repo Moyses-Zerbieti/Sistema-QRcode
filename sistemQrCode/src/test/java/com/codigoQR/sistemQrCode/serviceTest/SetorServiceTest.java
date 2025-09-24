0268b11f7a56299ef2f34da446b50a18fafd6485
Moyses-Zerbieti <zerbietimoyses@gmail.com>
2025-09-24 20:51:03 -0300

    test(service): adicionar testes unitários para SetorService

---
+++ sistemQrCode/src/test/java/com/codigoQR/sistemQrCode/serviceTest/SetorServiceTest.java
@@ -0,0 +1,86 @@
+package com.codigoQR.sistemQrCode.serviceTest;
+
+import com.codigoQR.sistemQrCode.exception.ResourceNotFoundException;
+import com.codigoQR.sistemQrCode.model.Setor;
+import com.codigoQR.sistemQrCode.repository.SetorRepository;
+import com.codigoQR.sistemQrCode.service.SetorService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class SetorServiceTest {
+
+    @Mock
+    SetorRepository setorRepository;
+
+    @InjectMocks
+    SetorService setorService;
+
+    @Test
+    void salvarSetorTest(){
+        Setor setorTest = new Setor();
+        setorTest.setNomeSetor("Financeiro");
+
+        when(setorRepository.save(any(Setor.class))).thenReturn(setorTest);
+
+        Setor retorno = this.setorService.salvarSetor(setorTest);
+
+        assertEquals("Financeiro", retorno.getNomeSetor());
+        verify(setorRepository, times(1)).save(setorTest);
+    }
+
+    @Test
+    void obterSetorPorIdTest(){
+        Setor setorTest = new Setor();
+        setorTest.setId(1);
+        setorTest.setNomeSetor("Financeiro");
+
+        when(setorRepository.findById(1)).thenReturn(Optional.of(setorTest));
+
+        Optional<Setor> retorno = setorService.obterPorId(1);
+
+        assertTrue(retorno.isPresent());
+        assertEquals("Financeiro", retorno.get().getNomeSetor());
+
+        verify(setorRepository, times(1)).findById(1);
+    }
+
+    @Test
+    void obterSetorPorIdNaoEncontradoTest(){
+
+        when(setorRepository.findById(99)).thenReturn(Optional.empty());
+
+        Optional<Setor> retorno = setorService.obterPorId(99);
+
+        assertFalse(retorno.isPresent());
+        verify(setorRepository, times(1)).findById(99);
+     }
+
+     @Test
+    void excluirSetorTest(){
+        when(setorRepository.existsById(1)).thenReturn(true);
+
+        setorService.excluirSetor(1);
+
+        verify(setorRepository,times(1)).deleteById(1);
+     }
+
+     @Test
+    void excluirSetorNaoExisteTest(){
+        when(setorRepository.existsById(99)).thenReturn(false);
+
+        assertThrows(ResourceNotFoundException.class,
+                ()-> setorService.excluirSetor(99));
+
+        verify(setorRepository, never()).deleteById(anyInt());
+    }
+}
