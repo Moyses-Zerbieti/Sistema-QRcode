237921a505dfcfe5310d7fbd401424a0e9ad7dfb
Moyses-Zerbieti <zerbietimoyses@gmail.com>
2025-10-07 18:29:05 -0300

    feat(config): adicionar configuração do OpenAPI para documentação do sistema TAG-ID
    Cria a classe `OpenApiConfiguration` responsável por definir as informações principais da API, como título, versão e contato do desenvolvedor.

--- sistemQrCode/src/main/java/com/codigoQR/sistemQrCode/config/OpenApiConfiguration.java
+++ sistemQrCode/src/main/java/com/codigoQR/sistemQrCode/config/OpenApiConfiguration.java
@@ -1,2 +1,21 @@
-package com.codigoQR.sistemQrCode.config;public class OpenApiConfiguration {
+package com.codigoQR.sistemQrCode.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Sistema de Cadastro TAG-ID",
+                version = "v1.1",
+                contact = @Contact (
+                        name = "Moyses Zerbieti",
+                        email = "zerbietimoyses@gmail.com",
+                        url = "https://www.linkedin.com/in/moyses-zerbieti/"
+                )
+        )
+)
+public class OpenApiConfiguration {
 }
