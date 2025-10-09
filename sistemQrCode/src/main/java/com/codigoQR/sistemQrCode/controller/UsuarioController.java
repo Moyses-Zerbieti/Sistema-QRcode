592c6aa6416db8fa9f8fa4802d538615648c95de
Moyses-Zerbieti <zerbietimoyses@gmail.com>
2025-10-09 19:31:32 -0300

    docs(swagger): adiciona documentação completa ao UsuarioController
    Foi incluída a documentação do controller responsável pelo gerenciamento de usuários utilizando as anotações do Swagger/OpenAPI.
    Foram adicionadas descrições detalhadas para os endpoints de criação, consulta e exclusão de usuários, incluindo os códigos de resposta HTTP e os respectivos significados.

--- sistemQrCode/src/main/java/com/codigoQR/sistemQrCode/controller/UsuarioController.java
+++ sistemQrCode/src/main/java/com/codigoQR/sistemQrCode/controller/UsuarioController.java
@@ -3,6 +3,10 @@ package com.codigoQR.sistemQrCode.controller;
 import com.codigoQR.sistemQrCode.genericController.GenericControllerUsuario;
 import com.codigoQR.sistemQrCode.model.Usuario;
 import com.codigoQR.sistemQrCode.service.UsuarioService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -11,6 +15,7 @@ import java.net.URI;
 
 @RestController
 @RequestMapping("usuario")
+@Tag(name = "Usuários")
 public class UsuarioController implements GenericControllerUsuario {
 
     private UsuarioService service;
@@ -22,6 +27,8 @@ public class UsuarioController implements GenericControllerUsuario {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     @PreAuthorize("hasRole('ADMIN')")
+    @Operation(summary = "Salvar", description = "Cadastrar um novo usuário")
+    @ApiResponse (responseCode = "201", description = "Usuário cadastrado com sucesso")
     public void salvar(@RequestBody Usuario usuario){
         service.salvar(usuario);
         URI location = headerLocationUsuario(usuario.getId());
@@ -29,6 +36,11 @@ public class UsuarioController implements GenericControllerUsuario {
 
     @GetMapping("{login}")
     @PreAuthorize("hasAnyRole('ADMIN,STAFF','USER')")
+    @Operation(summary = "Consulta", description = "Consulta de dados do Usuário")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Usuário encontrado"),
+            @ApiResponse(responseCode = "404", description = "Usuário não encontrado")
+    })
     public ResponseEntity<Usuario> consultaUsuario( @PathVariable String login){
         Usuario usuario = service.ObterPorLogin(login);
         if (usuario == null){
@@ -39,6 +51,8 @@ public class UsuarioController implements GenericControllerUsuario {
 
     @DeleteMapping("{login}")
     @PreAuthorize("hasRole('ADMIN')")
+    @Operation(summary = "Deletar", description = "Deletar um usuário do sistema")
+    @ApiResponse(responseCode = "201", description = "Usuário deletado com sucesso")
     public ResponseEntity<Void> deletarUsuarioLogin(@PathVariable String login){
         service.deletarUsuarioPorLogin(login);
         return ResponseEntity.noContent().build();
