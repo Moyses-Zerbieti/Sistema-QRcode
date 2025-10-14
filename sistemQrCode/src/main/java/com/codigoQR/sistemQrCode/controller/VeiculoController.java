f0af7cb51c4f35c084dbbf95d6e088ed685f150f
Moyses-Zerbieti <zerbietimoyses@gmail.com>
2025-10-14 16:10:04 -0300

    docs(controller): adiciona documentação detalhada Swagger ao VeiculoController
    As descrições foram aprimoradas e os códigos de resposta HTTP padronizados
    para melhorar a clareza e a integração com o Swagger UI.

--- sistemQrCode/src/main/java/com/codigoQR/sistemQrCode/controller/VeiculoController.java
+++ sistemQrCode/src/main/java/com/codigoQR/sistemQrCode/controller/VeiculoController.java
@@ -6,6 +6,10 @@ import com.codigoQR.sistemQrCode.dto.VeiculoResquest;
 import com.codigoQR.sistemQrCode.genericController.GenericControllers;
 import com.codigoQR.sistemQrCode.model.Veiculo;
 import com.codigoQR.sistemQrCode.service.VeiculoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +18,7 @@ import java.net.URI;
 
 @RestController
 @RequestMapping("veiculo")
+@Tag(name = "Veículo")
 public class VeiculoController implements GenericControllers {
 
     private VeiculoService veiculoService;
@@ -24,6 +29,12 @@ public class VeiculoController implements GenericControllers {
 
     @PostMapping
     @PreAuthorize("hasAnyRole('ADMIN','STAFF')")
+    @Operation(summary = "Cadastrar", description = "Realiza o cadastro de um veículo no sistema")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Veículo cadastrado com sucesso"),
+            @ApiResponse(responseCode = "409", description = "Já existe um veículo com esses dados no sistema"),
+            @ApiResponse(responseCode = "404", description = "Funcionario com o ID informado não foi cadastrado no sistema ")
+    })
     public ResponseEntity<?>salvar(@RequestBody VeiculoResquest veiculo){
         Veiculo salvo = veiculoService.salvarVeiculo(veiculo);
         URI location = gerarHeaderLocation(salvo.getId());
@@ -34,12 +45,22 @@ public class VeiculoController implements GenericControllers {
 
     @GetMapping("{id}")
     @PreAuthorize("hasAnyRole('ADMIN','STAFF','USER')")
+    @Operation(summary = "Consultar", description = "Executa a busca de um veículo no sistema pelo ID informado")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Veículo encontrado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Veículo não encontrado")
+    })
     public VeiculoResponseDTO consultarVeiculo(@PathVariable Integer id){
         return veiculoService.consultaVeiculo(id);
     }
 
     @DeleteMapping("{id}")
     @PreAuthorize("hasRole('ADMIN')")
+    @Operation(summary = "Deletar", description = "Deleta um veículo do sistema")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "Veículo deletado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Véiculo não cadastrado no sistema")
+    })
     public ResponseEntity<?> deletarVeiculo(@PathVariable Integer id){
         veiculoService.deletarVeiculo(id);
     return ResponseEntity.noContent().build();
@@ -47,6 +68,12 @@ public class VeiculoController implements GenericControllers {
 
     @PutMapping("/{id}")
     @PreAuthorize("hasAnyRole('ADMIN','STAFF')")
+    @Operation(summary = "Atulizar dados", description = "Atualiza os dados do veículo")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Dados atualizados com sucesso"),
+            @ApiResponse(responseCode = "409", description = "Já existe um veículo com esses dados no sistema"),
+            @ApiResponse(responseCode = "404", description = "Veículo não cadastrado no sistema para alteração")
+    })
     public ResponseEntity<?> atualizarDadosVeiculo(
             @PathVariable  Integer id,
             @RequestBody AtualizacaoVeiculoDTO veiculonovo){
