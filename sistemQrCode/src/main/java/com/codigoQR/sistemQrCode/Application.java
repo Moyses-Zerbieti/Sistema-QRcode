dd392a920edd437fc51f0af49a9031d493e742be
Moyses-Zerbieti <zerbietimoyses@gmail.com>
2025-11-16 11:56:29 -0300

    .

--- sistemQrCode/src/main/java/com/codigoQR/sistemQrCode/Application.java
+++ sistemQrCode/src/main/java/com/codigoQR/sistemQrCode/Application.java
@@ -15,7 +15,6 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplicationBuilder builder = new SpringApplicationBuilder(Application.class);
 		builder.bannerMode(Banner.Mode.OFF);
-		builder.profiles("Moyses Zerbieti");
 		builder.run(args);
 
 		ConfigurableApplicationContext applicationContext = builder.context();
